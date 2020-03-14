266dfbd98ffc978fe83f89ec6c15211519bb5567
Gowthamnarayanan S <gossgowtham@gmail.com>
2020-03-14 20:11:28 +0530

    Updates

---
+++ precache-manifest.4677b1b9c788f6d8e4e62af0c8bf4b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c37ab0085a385fb715c14f9fb738f9",
+    "url": "/nakshathradentistry/index.html"
+  },
+  {
+    "revision": "1ce0ee08776594b265a8",
+    "url": "/nakshathradentistry/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "325f6cf7b0ad02ad5b3c",
+    "url": "/nakshathradentistry/static/js/2.cd858ba0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nakshathradentistry/static/js/2.cd858ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce0ee08776594b265a8",
+    "url": "/nakshathradentistry/static/js/main.d8a25304.chunk.js"
+  },
+  {
+    "revision": "f8a44689addb3323b9b2",
+    "url": "/nakshathradentistry/static/js/runtime-main.e631e63a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nakshathradentistry/static/media/logo.5d5d9eef.svg"
+  }
+]);
